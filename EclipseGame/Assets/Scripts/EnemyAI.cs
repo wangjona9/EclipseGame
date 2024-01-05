e792340afe4eea14110035315629891c21441b96
FortuneCookie07 <fortuneplays999@gmail.com>
2024-01-04 21:31:39 -0600

    Fixed Monster Prefabs

--- EclipseGame/Assets/Scripts/EnemyAI.cs
+++ EclipseGame/Assets/Scripts/EnemyAI.cs
@@ -42,7 +42,7 @@ public class EnemyAI : MonoBehaviour
             ChasePlayer();
         if (playerInAttackRange && playerInSightRange)
             AttackPlayer();
-            // nigga
+            
 
     }
 
